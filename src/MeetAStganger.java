567ec9cd6e70f241c052a96e7c5f19968a993e7d
Professional <proleichik.ivan@gmail.com>
2022-08-20 21:07:27 +0300

    Initial commit

---
+++ src/MeetAStganger.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class MeetAStganger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println("Hello, " + input);
+    }
+}
